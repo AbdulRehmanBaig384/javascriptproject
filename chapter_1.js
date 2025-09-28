d97ab76c840400b8b5f64e34f17a2ec2db3de271
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-28 14:11:29 +0500

    Update chapter_1.js

--- chapter_1.js
+++ chapter_1.js
@@ -26,3 +26,49 @@ function javascript_question4b(){
 }
 javascript_question4();
 javascript_question4b();
+
+let input_element=document.getElementById("todoinput");
+
+let input_addbutton=document.getElementById("btn");
+
+let input_tasklist=document.getElementById("tasklist");
+
+input_addbutton.addEventListener('click',()=>{
+    const task_text=input_element.value.trim();
+    
+    if(task_text===''){
+        
+        alert("task cant be empty");
+    return;
+    }
+    const listitem=document.createElement("li");
+    
+    const taskspan=document.createElement('span');
+    
+    taskspan.textContent=task_text;
+
+    taskspan.addEventListener('click',()=>{
+        
+  taskspan.classList.toggle("completed");
+        
+    });
+    const delete_button=document.createElement("button");
+    
+    delete_button.textContent="delete";
+    
+    delete_button.style.marginLeft="10px";
+    
+    delete_button.addEventListener('click',()=>{
+        
+        listitem.remove();
+    });
+    listitem.appendChild(taskspan);
+    
+    listitem.appendChild(delete_button);
+
+    input_tasklist.appendChild(listitem);
+    
+    input_element.value='';
+
+})
+
