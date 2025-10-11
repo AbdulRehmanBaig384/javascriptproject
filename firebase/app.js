d2bdff138bfa54636e7b01b781536a681d435469
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-10-11 08:55:46 +0500

    Update app.js

--- firebase/app.js
+++ firebase/app.js
@@ -1,7 +1,7 @@
-// let user_input=prompt("Enter the city name");
-// let country=["karachi","islamabad","lahore","peshawar"];
-// let match=false;
-// for(let i=0;i<country.length-1;i++){
+let user_input=prompt("Enter the city name");
+let country=["karachi","islamabad","lahore","peshawar"];
+let match=false;
+for(let i=0;i<country.length-1;i++){
     if(user_input==country[i]){
         match=true;
         document.write("city found");
@@ -10,6 +10,7 @@
 }
 if(match==false){
     document.write("City not found");
+
 
 
 
